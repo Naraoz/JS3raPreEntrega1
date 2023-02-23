95bed084cf22c5814acea8f6ddef7a69ec9e622c
Nicolas <araoznicolas@gmail.com>
2023-02-22 21:41:42 -0300

    first commit

---
+++ js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Cupcake",
+    precio: 300,
+    img: "img/Cupcake.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Cookie",
+    precio: 250,
+    img: "img/Cookies1.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Cake",
+    precio: 1500,
+    img: "img/Cake1.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Brownie",
+    precio: 800,
+    img: "img/Brownies.jpg",
+    cantidad: 1,
+  },
+];
